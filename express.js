57986e020706700ac74614134eebb22bf7ed2c03
Stefan Savić <stefansavic072@gmail.com>
2024-07-30 17:20:54 +0200

    Route parameters

--- express.js
+++ express.js
@@ -2,21 +2,14 @@ const express = require("express");
 
 const app = express();
 
-/**
- * In order for our GET /messages request to match the /messages route, we will need to reverse the order our routes are defined.
- * Doing so will prevent it from reaching the * route, as it will match the /messages route first.
- */
-
-app.get("*", (req, res) => {
-  res.send(
-    "* is a great way to catch all otherwise unmatched parts, e.g. for custom 404 error handling."
-  );
+app.get("/:username/message", (req, res) => {
+  console.log(req.params); // {username : e.g. 'Stefan'}
+  res.end();
 });
 
-app.get("/messages", (req, res) => {
-  res.send(
-    "This route will not be reached because the previous route path mathces first."
-  );
+app.get("/:username/message/:messageId", (req, res) => {
+  console.log(req.params); //{username : e.g. 'Stefan', messageId: '16' }
+  res.end();
 });
 
 app.listen(8000);
