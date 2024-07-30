a256e8014cd18730056036d9aece81a2bbbf5668
Stefan Savić <stefansavic072@gmail.com>
2024-07-30 17:28:32 +0200

    Query Parameters and req.query

--- express.js
+++ express.js
@@ -2,14 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.get("/:username/message", (req, res) => {
-  console.log(req.params); // {username : e.g. 'Stefan'}
-  res.end();
-});
+/**
+ * GET /odin/messages?sort=date&direction=ascending will log
+ * Params: { username: "odin" }
+ * Query: { sort: "date", direction: "ascending" }
+ *
+ * GET /odin/messages?sort=date&sort=likes&direction=ascending will log
+ * Params: { username: "odin" }
+ * Query: { sort: ["date", "likes"], direction: "ascending" }
+ */
 
-app.get("/:username/message/:messageId", (req, res) => {
-  console.log(req.params); //{username : e.g. 'Stefan', messageId: '16' }
-  res.end();
+app.get("/:username/message", (req, res) => {
+  console.log("Params:", req.params);
+  console.log("Query:", req.query);
 });
 
 app.listen(8000);
