f44607b7c9fd7ec491874f46c431c45b7628176a
Stefan Savić <stefansavic072@gmail.com>
2024-07-30 21:32:16 +0200

    Controllers

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -1,17 +1,14 @@
-const getUserById = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getUserById = asyncHandler(async (req, res) => {
   const userId = req.params.id;
 
-  try {
-    const user = await someDBQueryToGetUser(userId);
+  const user = await someDBQueryToGetUser(userId);
 
-    if (!user) {
-      res.status(404).send("User not found");
-      return;
-    }
-
-    res.send(`User found: ${user.name}`);
-  } catch (error) {
-    console.error("Error retrieving user:", error);
-    res.status(500).send("Internal Server Error");
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
   }
-};
+
+  res.send(`User found: ${user.name}`);
+});
