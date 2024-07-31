bf6add3e2fda440b969a0d4e0570a302cae79983
Stefan Savić <stefansavic072@gmail.com>
2024-07-31 18:10:09 +0200

    Setting up EJS

--- custom-error.js
+++
@@ -1,8 +0,0 @@
-class CustomNotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.statudsCode = 404;
-
-    this.name = "NotFoundError";
-  }
-}
--- express-app/app.js
+++ express-app/app.js
@@ -1,7 +1,12 @@
 const exports = require("express");
 const userRouter = require("./routes/userRoutes");
+const path = require("path");
 
 const app = express();
+
+//This enables EJS as the view engine, and that our app should look for templates in the /views subdirectory.
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // parses form payloads and sets it to the 'req.boy'
 app.use(express.urlencoded({ extended: false }));
