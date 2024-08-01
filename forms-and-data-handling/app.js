3f94bafd8febd269570f83d42c6acedc1c0b72d7
Stefan Savić <stefansavic072@gmail.com>
2024-08-01 22:06:48 +0200

    Adding app.js

---
+++ forms-and-data-handling/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/userRouter");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", userRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}`));
